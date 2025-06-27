6c37aee937776f49def6a2aded77b9392dd4a29d
David <drs45@bath.ac.uk>
2025-06-27 14:54:01 +0100

    Add: new feature or fix

---
+++ data/synhtetic_data1_generation/Bates/generate.py
@@ -0,0 +1,25 @@
+# generate.py
+
+"""
+Generate Stage 1 synthetic dataset for Bates model.
+Saves file to:
+C:/Users/david/BathUni/MA50290_24/model_calibration/data/synthetic_data1/Bates/
+"""
+
+import sys
+import os
+
+# Dynamically add model_calibration/ to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
+sys.path.append(project_root)
+
+from common.synthetic_data1 import generate_stage1_dataset
+
+if __name__ == "__main__":
+    generate_stage1_dataset(
+        model_type="bates",
+        n_param_sets=1000,
+        n_paths_per_set=10,
+        save_path=r"C:\Users\david\BathUni\MA50290_24\model_calibration\data\synthetic_data1\Bates\stage1_bates_dataset.npz",
+        seed=42
+    )
---
+++ data/synhtetic_data1_generation/MJD/generate.py
@@ -0,0 +1,25 @@
+# generate.py
+
+"""
+Generate Stage 1 synthetic dataset for MJD model.
+Saves file to:
+C:/Users/david/BathUni/MA50290_24/model_calibration/data/synthetic_data1/MJD/
+"""
+
+import sys
+import os
+
+# Dynamically add model_calibration/ to sys.path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
+sys.path.append(project_root)
+
+from common.synthetic_data1 import generate_stage1_dataset
+
+if __name__ == "__main__":
+    generate_stage1_dataset(
+        model_type="mjd",
+        n_param_sets=1000,
+        n_paths_per_set=10,
+        save_path=r"C:\Users\david\BathUni\MA50290_24\model_calibration\data\synthetic_data1\MJD\stage1_mjd_dataset.npz",
+        seed=42
+    )
